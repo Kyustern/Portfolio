e5fb6f0ddc12c53fb1df97fa0f04d0c962be369e
Kyustern <leont562@gmail.com>
2020-05-07 09:56:29 +0200

    updating client build for production

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0963888872f150a43b6adc5b54ecb9b.js"
+  "/precache-manifest.3ce47d33deba65b3eee6198443e5dd47.js"
 );
 
 self.addEventListener('message', (event) => {
